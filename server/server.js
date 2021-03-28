a032011f3eaa836cea3a9ba3acad080837f49011
motorcykle <mohamedabdulridha1@gmail.com>
2021-03-28 12:26:49 +0200

    server setup 2

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 const mongoURI = process.env.MONGO_URI;
 
+
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
